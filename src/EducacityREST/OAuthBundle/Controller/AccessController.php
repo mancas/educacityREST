3d9bdd798bcba9acfd5728adea8525000f9bc335
manu <manu@manu-VirtualBox.(none)>
2014-09-01 15:30:58 +0200

    register

--- src/EducacityREST/OAuthBundle/Controller/AccessController.php
+++ src/EducacityREST/OAuthBundle/Controller/AccessController.php
@@ -2,6 +2,7 @@
 
 namespace EducacityREST\OAuthBundle\Controller;
 
+use EducacityREST\UserBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\SecurityContext;
@@ -28,6 +29,89 @@ class AccessController extends Controller
 
         $lastUsername = (null === $session) ? '' : $session->get(SecurityContext::LAST_USERNAME);
 
-        return $this->render('OAuthBundle:Access:login.html.twig', array('last_username' => $lastUsername, 'error' => $error));
+        return $this->render(
+            'OAuthBundle:Access:login.html.twig',
+            array(
+                'last_username' => $lastUsername,
+                'error' => $error,
+            )
+        );
+    }
+
+    public function loginCheckAction(Request $request)
+    {
+
+    }
+
+    public function postLoginAction(Request $request)
+    {
+        $username = $request->get('username');
+        $password = $request->get('password');
+
+        $user = $this->get('user.handler')->get(null, $username);;
+
+        if(!($user instanceof User)){
+            $jsonResponse = json_encode(array('code' => 404, 'error' => 'User not exists'));
+            $response = new \Symfony\Component\HttpFoundation\Response($jsonResponse);
+            $response->headers->set('Content-Type', 'application/json');
+
+            return $response;
+        }
+        if(!$this->checkUserPassword($user, $password)){
+            $jsonResponse = json_encode(array('code' => 404, 'error' => 'Wrong password'));
+            $response = new \Symfony\Component\HttpFoundation\Response($jsonResponse);
+            $response->headers->set('Content-Type', 'application/json');
+
+            return $response;
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $clients = $em->getRepository('OAuthBundle:Client')->findAll();
+        $client = $clients[0];
+        $jsonResponse = json_encode(array('code' => 200, 'client_id' => $client->getPublicId(),
+            'client_secret' => $client->getSecret));
+
+        $response = new \Symfony\Component\HttpFoundation\Response($jsonResponse);
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+
+    protected function checkUserPassword(User $user, $password)
+    {
+        $factory = $this->get('security.encoder_factory');
+        $encoder = $factory->getEncoder($user);
+        if(!$encoder){
+            return false;
+        }
+        $passwordEncoded = $encoder->encodePassword($password, $user->getSalt());
+        ldd($passwordEncoded, $user->getPassword(), $password);
+        return $encoder->isPasswordValid($user->getPassword(), $password, $user->getSalt());
+    }
+
+    public function setUserAction(Request $request)
+    {
+        $accessToken = $request->get('access_token');
+        $refreshToken = $request->get('refresh_token');
+        $email = $request->get('email');
+
+        $em = $this->getDoctrine()->getManager();
+        $accessTokenEntity = $em->getRepository('OAuthBundle:AccessToken')->findOneByToken($accessToken);
+        $refreshTokenEntity = $em->getRepository('OAuthBundle:RefreshToken')->findOneByToken($refreshToken);
+        $user = $em->getRepository('UserBundle:User')->findOneByEmail($email);
+
+        $accessTokenEntity->setUser($user);
+        $refreshTokenEntity->setUser($user);
+
+        $em->persist($accessTokenEntity);
+        $em->persist($refreshTokenEntity);
+        $em->flush();
+
+        $jsonResponse = json_encode(array('code' => 200));
+
+        $response = new \Symfony\Component\HttpFoundation\Response($jsonResponse);
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
     }
 }
--- src/EducacityREST/RegisterBundle/Controller/RegisterController.php
+++ src/EducacityREST/RegisterBundle/Controller/RegisterController.php
@@ -2,6 +2,7 @@
 namespace EducacityREST\RegisterBundle\Controller;
 
 use Doctrine\Common\Collections\ArrayCollection;
+use EducacityREST\OAuthBundle\Command\CreateOAuthClientCommand;
 use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\Form\Exception\InvalidArgumentException;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,12 +19,18 @@ use FOS\RestBundle\Controller\Annotations\QueryParam;
 use Symfony\Component\HttpFoundation\Response;
 
 class RegisterController extends FOSRestController{
-
+//access
+//Yzk0MjE1YmVjZWU1YzIyYTAzZTY1ZWEwNzkxNjIwMGQ1NjRlNDgzOTI2NTU3M2RmY2QyNGRkMzBkNzVjMWI4NQ
+//refresh NjFjOTFkYzJjYmQ2NmZjNWIwZmNlNDNmOGExMDRjOGI3Y2I3NjZkMDg4ZDIxOTBkZWVmNjdlNWU1ZWUzYTdiNA
     public function postRegisterAction(Request $request)
     {
         $result = $this->get('user.handler')->post($request);
         if ($result instanceof User) {
-            $jsonResponse = json_encode(array('code' => 200, 'email' => $result->getEmail()));
+            $em = $this->getDoctrine()->getManager();
+            $clients = $em->getRepository('OAuthBundle:Client')->findAll();
+            $client = $clients[0];
+            $jsonResponse = json_encode(array('code' => 200, 'client_id' => $client->getPublicId(),
+                                              'client_secret' => $client->getSecret()));
             $response = new \Symfony\Component\HttpFoundation\Response($jsonResponse);
             $response->headers->set('Content-Type', 'application/json');
         } else {
@@ -31,6 +38,19 @@ class RegisterController extends FOSRestController{
         }
 
         return $response;
+    }
+
+    public function testAction()
+    {
+        require_once 'Text/Wiki/Mediawiki.php';
+        $wiki = new \Text_Wiki_Mediawiki();
+        //$wiki = $wiki->factory('Mediawiki');
+        $wiki->setRenderConf('xhtml', 'Wikilink', 'view_url',
+            'http://wikipedia.org/wiki/');
+        $wiki->setRenderConf('xhtml', 'Wikilink', 'pages', false);
+        $wiki->setRenderConf('xhtml', 'Url', 'target', false);
+        ldd($wiki->transform("{{other uses|La Giralda (disambiguation)}} {{Infobox World Heritage Site | WHS = [[Seville Cathedral|Cathedral]], Alcázar and [[Archivo General de Indias|General Archive of the Indies]] in Seville | Image = [[File:Sevilla La Giralda 18-03-2011 18-24-31.jpg|200px]]<br><small>La Giralda</small> | State Party = [[Spain]] | Type = Cultural | Criteria = i, ii, iii, vi | ID = 383 | Region = [[List of World Heritage Sites in Europe|Europe and North America]] | Year = 1987 | Session = 11th | Link = http://whc.unesco.org/en/list/383 }} The \'\'\'Giralda\'\'\' ({{lang-es|La Giralda}} ; {{lang-ar|الخيرالدة}}) is a former [[minaret]] that was converted to a [[bell tower]]", 'Xhtml'));
+        return $this->render("RegisterBundle:Default:index.html.twig");
     }
     
     /**
